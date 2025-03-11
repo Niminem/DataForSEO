1d23f086e9707cb9c20ebfd03179715c641e1d13
Niminem <leon.l.lysak@gmail.com>
2025-03-11 01:29:42 -0400

    added quick example, updated endpoints

---
+++ examples/keywords_for_site.nim
@@ -0,0 +1,38 @@
+# https://docs.dataforseo.com/v3/dataforseo_labs/google/keywords_for_site/live/?bash
+# Keywords For Site
+#
+# The Keywords For Site endpoint will provide you with a list of keywords relevant to the
+# target domain. Each keyword is supplied with relevant categories, search volume data for
+# the last month, cost-per-click, competition, and search volume trend values for the
+# past 12 months.
+
+# Datasource: DataForSEO Keyword Database segmented by relevant domains from Google
+# Ads API and supplemented by data from DataForSEO SERP Database.
+#
+# Search algorithm: relevance-based search for keywords that fall into the same category
+# as the target domain.
+#
+# Examples:
+# Specified target domain:
+# “letslevitate.com”
+# Resulting keyword ideas:
+# •”xvt blades”,
+# •”scar blades falcon”,
+# •”palatine blades”,
+# •”blades macomb il”
+
+import std/[json, strutils]
+import ../src/dataforseo
+
+let creds = readFile("creds.txt").splitWhitespace()
+var client = newDFSClient(login=creds[0], password=creds[1])
+
+var data = %*[{"language_name": "English", "location_name": "United States"}]
+data[0]["target"] = newJstring "seo.science"
+data[0]["include_clickstream_data"] = newJbool true # doubles price from 0.011 to 0.021
+data[0]["limit"] = newJint 10
+data[0]["offset"] = newJint 0
+
+let response = client.fetch(KeywordsForSite, data)
+writeFile("keywords_for_site.json", response.body.parseJson().pretty(2))
+client.close()
--- src/dataforseo/endpoints.nim
+++ src/dataforseo/endpoints.nim
@@ -15,4 +15,33 @@ const
         EndpointBase & "keywords_data/clickstream_data/bulk_search_volume/live")
     # ------------------------------------------------------
     
+    # ----- DataForSEO Labs API -> Google -> Keyword Research -----
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/keywords_for_site/live/?bash
+    KeywordsForSite*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/keywords_for_site/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/related_keywords/live/?bash
+    RelatedKeywords*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/related_keywords/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/keyword_suggestions/live/?bash
+    KeywordSuggestions*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/keyword_suggestions/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/keyword_ideas/live/?bash
+    KeywordIdeas*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/keyword_ideas/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/historical_search_volume/live/?bash
+    HistoricalSearchVolume*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/historical_search_volume/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/bulk_keyword_difficulty/live/?bash
+    BulkKeywordDifficulty*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/bulk_keyword_difficulty/live")
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/search_intent/live/?bash
+    SearchIntent*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/search_intent/live")
+    # ----------------------------------------------------------------
+
+    # ----- DataForSEO Labs API -> Google -> Competitor Research -----
+    # https://docs.dataforseo.com/v3/dataforseo_labs/google/historical_bulk_traffic_estimation/live/
+    HistoricalBulkTrafficEstimation*: DFSRequest = (HttpPost,
+        EndpointBase & "dataforseo_labs/google/historical_bulk_traffic_estimation/live")
+
     # TODO: ***Add more endpoints here ***
